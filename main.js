7e01b503f7f32b529f250966e18d47f3137137f7
seb-faull <seb_faull@hotmail.co.uk>
2017-09-11 21:59:59 +0100

    Got the play computer mode working

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
+//Variables
 var gameRunning = true;
 var player1Wins = false;
 var player2Wins = false;
-var computerChoice = Math.random();
+var computerChoice = Math.floor(Math.random() * 3) + 1;
 
+
+//Functions
 function rules () {
 	var rules = alert('Rock, Paper, Scissors (aka "Ro-Sham-Bo", janken, "Bato, Bato, Pick" and "Scissors, Paper, Stone") is a simple hand game that is played around the world, with many different names and variations. \n \nIt is commonly used as a way of coming to decisions, and in some cases is even played for sport. The rules require that competing players use one hand to form one of three shapes at an agreed-upon time. The person that plays the strongest object is the winner of the game. It\'s that easy!');
 }
 
 function getInput (playerName) {
-	prompt('Would you like to play in Computer mode or 2 player mode? \nComputer mode - \'y\' \n2 player mode - \'n\'');
 	return prompt(playerName + ', please select either \nRock: r \nPaper: p \nScissors: s')
 } 
 
@@ -22,8 +24,18 @@ function twoWins() {
 	alert('Player 2 wins!');
 }
 
-while (gameRunning = true) {
-	rules();
+function computerWins() {
+	alert('Computer wins!');
+}
+
+//Program Starts
+rules();
+
+var playMode = prompt('Would you like to play in Computer mode or 2 player mode? \nComputer mode - \'y\' \n2 player mode - \'n\'');
+
+if (playMode === 'n') {
+
+	while (gameRunning = true) {
 	var player1 = getInput('Player 1')
 	var player2 = getInput('Player 2')
 
@@ -49,6 +61,48 @@ while (gameRunning = true) {
 				oneWins();
 			}
 		}
+	}
+}
+
+if (playMode === 'y') {
+
+	if (computerChoice === 1) {
+		computerChoice = 'r';
+	} else if (computerChoice === 2) {
+		computerChoice = 'p';
+	} else if (computerChoice === 3) {
+		computerChoice = 's';
+	} else {
+		alert('Computer bugged out!');
+	} 
+
+	while (gameRunning = true) {
+	var player1 = getInput('Player 1')
+	var computerChoice;
+
+	if (player1 === computerChoice) {
+		alert('It is a draw!');
+
+	} else if (player1 === 'r') {
+		if (computerChoice === 2) {
+			computerWins();
+		} else {
+			oneWins();
+		}
+	}	else if (player1 === 'p') {
+			if (computerChoice === 3) {
+				computerWins();
+			} else {
+				oneWins();
+				}
+			} else if (player1 === 's') {
+			if (computerChoice === 1) {
+					computerWins();
+			} else {
+				oneWins();
+			}
+		}
+	}
 }
 
 
